d9052c280c144029b6e6bb58fcd7936088628515
Patrice Drayton <draytonpatrice@marilynssecret.org>
2018-12-27 20:04:51 +0000

    Automatically backed up by Learn

---
+++ lib/dog.rb
@@ -0,0 +1,71 @@
+class Shelter   
+  attr_accessor :name, :city
+  @@all = []
+
+  def initialize (name, city)
+    @name = name
+    @city = city
+    @dogs = [] 
+    @@all << self 
+  end 
+  
+  def add_dog(dog)
+    dog.shelter = self
+    @dogs << dog 
+  end 
+  
+  def dogs 
+    @dogs 
+  end 
+  
+  def self.all 
+    @@all
+  end 
+end 
+
+class Dog  
+  attr_accessor :name, :age, :breed, :shelter, :owner
+  @@all = []
+  
+  def initialize(name, age, breed)
+    @name = name
+    @age = age
+    @breed = breed 
+    @@all << self 
+  end 
+  
+  def self.all
+    @@all
+  end 
+end 
+
+
+class Owner 
+  attr_accessor :name, :age
+  attr_reader :dogs 
+  @@all = []
+  
+   def initialize (name, age)
+    @name = name
+    @age = age
+    @dogs = []
+    @shelters = []
+    @@all << self 
+  end 
+  
+  def add_dog(dog)
+    @dogs << dog 
+    dog.owner = self 
+  end 
+  
+  def shelters
+    self.dogs.each do |dog| 
+      @shelters << dog.shelter if dog.shelter
+    end 
+    @shelters
+  end
+  
+  def self.all 
+    @@all
+  end 
+end
